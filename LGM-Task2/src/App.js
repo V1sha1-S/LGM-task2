0d4fc3350bc5a8f822886e39c9958a2c75d8996b
vishal naik <76083887+V1sha1-S@users.noreply.github.com>
2022-08-30 12:09:10 +0530

    Add files via upload

---
+++ LGM-Task2/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const App = () => {
+  const [users, setUser] = useState([]);
+
+  const loadUsers = async () => {
+    // console.log("before");
+    const response = await fetch("https://api.github.com/users");
+    const jsonResponse = await response.json();
+    setUser(jsonResponse);
+  };
+
+  return (
+    <div className="App">
+      <h1> Hello All</h1>
+      <button onClick={loadUsers}>Get Users</button>
+
+      <h2>Users:</h2>
+      <ul>
+        {users.map(({ id,login,avatar_url }) => (
+          <li key={id}>
+            Name: {login}, Avatar: {avatar_url}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
